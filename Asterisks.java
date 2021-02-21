3fd7831ecc5ce09cf7e6345ddf72490159a238d7
Joy Udom <judom.2011@gmail.com>
2021-02-21 23:52:19 +0000

    asterisks

---
+++ Asterisks.java
@@ -0,0 +1,10 @@
+public class Asterisks{
+
+	public static void main (String [] args){
+
+	System.out.printf("%s%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n", "*********","*       *","*       *","*       *","*       *","*       *","*       *","*       *","*********");
+	System.out.printf("%s%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n", " *** ", " *     * ", " *       * ", " *       * "," *       * "," *       * "," *       * "," *     * "," *** ");
+	System.out.printf("%s%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n", "  *  ", " *** ","*****" , "  *  ","  *  ","  *  ","  *  ","  *  ","  *  "); 	
+	System.out.printf("%s%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n", "    *    ", "   * *   ", "  *   *  ", " *     * ", "*       *", " *     * ",  "  *   *  ","   * *   ", "    *    ");
+}
+}
