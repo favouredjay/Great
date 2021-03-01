02ac0ed5e0f4f615aa812f62654c87e53a66d0f6
Joy Udom <judom.2011@gmail.com>
2021-03-01 20:05:51 +0000

    chapter 2 exercises

---
+++ AverageDoWhile.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class AverageDoWhile {
+
+    public static void main(String[] args){
+        Scanner scam = new Scanner(System.in);
+
+        double score = 0;
+        double sum;
+        double result = 0;
+
+        do {
+            System.out.println("Type in a number and press enter");
+            sum = scam.nextDouble();
+            if (sum >= 1 && sum<=100) {
+                result += sum;
+                score++;
+            }
+        }
+        while(score < 10);
+        double average = result / (score)*1.0;
+        System.out.println("the total average is " + average);
+
+
+
+    }
+
+}
+
+
